ab889e83e397146f3965f74422607439059eb96d
snehaljha <snehaljha08@gmail.com>
2021-12-21 02:02:26 +0530

    added separate theming class

---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+@immutable
+class AppColors {
+  //page components
+  final Color pageTitleText = const Color(0x000000ff);
+
+  //lists component
+  final Color renameActionBackground = const Color(0x888888ff);
+  final Color actionForeground = const Color(0x000000ff);
+  final Color deleteActionBackround = const Color(0xff3333ff);
+
+  const AppColors();
+}
---
+++ lib/theme/app_theme.dart
@@ -0,0 +1,5 @@
+import 'package:moor_lists/theme/app_colors.dart';
+
+class AppTheme {
+  static const AppColors colors = AppColors();
+}
