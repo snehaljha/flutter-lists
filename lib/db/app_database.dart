85b105fd3ca7925b09023af77f50c1e3155b31c1
snehaljha <snehaljha08@gmail.com>
2021-12-22 00:46:38 +0530

    added tasks stream

--- lib/db/app_database.dart
+++ lib/db/app_database.dart
@@ -5,7 +5,9 @@ part 'app_database.g.dart';
 
 @UseMoor(tables: [Tasks])
 class AppDatabase extends _$AppDatabase {
-  AppDatabase() : super(FlutterQueryExecutor.inDatabaseFolder(path: 'moorList.sqlite', logStatements: true));
+  AppDatabase()
+      : super(FlutterQueryExecutor.inDatabaseFolder(
+            path: 'moorList.sqlite', logStatements: true));
   int get schemaVersion => 1;
 
   Future<List<String?>> getTitles() {
@@ -17,12 +19,17 @@ class AppDatabase extends _$AppDatabase {
     final query = selectOnly(tasks, distinct: true)..addColumns([tasks.title]);
     return query.map((row) => row.read(tasks.title)).watch();
   }
-  
-  Future<List<Task>> getTasks(String title) => (select(tasks)..where((task) => task.title.equals(title))).get();
+
+  Future<List<Task>> getTasks(String title) =>
+      (select(tasks)..where((task) => task.title.equals(title))).get();
+
+  Stream<List<Task>> watchTasks(String title) =>
+      (select(tasks)..where((task) => task.title.equals(title))).watch();
 
   Future<int> addTask(Task task) => into(tasks).insert(task);
 
-  Future<int> deleteTitle(String title) => (delete(tasks)..where((task) => task.title.equals(title))).go();
+  Future<int> deleteTitle(String title) =>
+      (delete(tasks)..where((task) => task.title.equals(title))).go();
 
   Future<int> deleteTask(Task task) => delete(tasks).delete(task);
 }
